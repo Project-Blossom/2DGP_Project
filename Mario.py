9d7188b968b4956c66d50959c222443c174cf821
Project-Blossom <checklist0626@gmail.com>
2022-10-16 22:19:00 +0900

    3주차 아이템 구현
    패널 추가, 아이템 움직임 추가, 큰 마리오 리소스 편집

--- Mario.py
+++ Mario.py
@@ -4,23 +4,32 @@ import main as screen
 import item_state
 VELOCITY = 1 # 속도
 MASS = 1000 # 질량
+class Tile:
+    def __init__(self):
+        self.image = load_image('Tiles.png')
+    def draw(self):
+        self.draw()
 
 class Mario:
     def __init__(self): # 초기화
         self.image = load_image('SmallMario.png')
         self.dir_x = 0
         self.x, self.y = screen.WIDTH // 2, 35 # 초기 위치 (화면 하단 중앙)
-        self.rect = None
         self.pose = 0
         self.frame = 0
         self.isJump = 0 # 점프 확인
         self.v = VELOCITY # 속도
         self.m = MASS # 질량
+        self.life = 1
+        self.state = None
 
     def draw(self): #그리기
         if self.dir_x == 1: # 오른쪽을 향할때
             if self.isJump > 0:
-                self.image.clip_draw(600, 91, 100, 75, self.x, self.y)
+                if self.state == 'mushroom':
+                    self.image.clip_draw(600, 91, 100, 75, self.x, self.y)
+                else:
+                    self.image.clip_draw(600, 91, 100, 75, self.x, self.y)
             else:
                 self.image.clip_draw(self.frame * 100 + 100, self.pose + 91, 100, 75, self.x, self.y)
             self.pose = -1
@@ -28,14 +37,22 @@ class Mario:
             if self.isJump > 0:
                 self.image.clip_draw(700, 335, 100, 75, self.x, self.y)
             else:
-                self.image.clip_draw(self.frame * 100 + 1000, self.pose+12, 100, 75, self.x, self.y)
+                if self.state == 'mushroom':
+                    self.image = load_image('BigMario.png')
+                    self.image.clip_draw(self.frame * 100 + 1000, self.pose + 12, 100, 75, self.x, self.y)
+                else:
+                    self.image.clip_draw(self.frame * 100 + 1000, self.pose + 12, 100, 75, self.x, self.y)
             self.pose = 320
         elif self.dir_x == 0: # 스탠딩
             self.pose += 92
             if self.isJump > 0:
                 self.image.clip_draw(600, 91, 100, 75, self.x, self.y)
             else:
-                self.image.clip_draw(0, self.pose, 100, 75, self.x, self.y)
+                if self.state == 'mushroom':
+                    self.image = load_image('BigMario.png')
+                    self.image.clip_draw(0, self.pose, 100, 75, self.x, self.y)
+                else:
+                    self.image.clip_draw(0, self.pose, 100, 75, self.x, self.y)
             self.pose -= 92
 
     def screen_check(self): # 화면 밖으로 못나가게 하기
@@ -79,7 +96,7 @@ class Mario:
 
 class Mushroom:
     def __init__(self):
-        self.x = screen.WIDTH // 2
+        self.x = screen.WIDTH // 2 - 300
         self.y = screen.HEIGHT - 20
         self.image = load_image('Items.png')
         self.dir_x = -0.6
@@ -102,6 +119,76 @@ class Mushroom:
         elif self.x > screen.WIDTH:
             self.x = screen.WIDTH
             self.dir_x = -0.6
+
+class Fire_Flower:
+    def __init__(self):
+        self.x = screen.WIDTH // 2
+        self.y = screen.HEIGHT - 20
+        self.image = load_image('Items.png')
+        self.dir_x = -0.6
+
+    def draw(self):
+        self.image.clip_draw(190, 0, 100, 60, self.x, self.y)
+
+    def update(self):
+        if self.y > 35:
+            self.y -= 1
+
+    def screen_check(self):
+        if self.y < 35:
+            self.y = 35
+
+class Star:
+    def __init__(self):
+        self.image = load_image('Items.png')
+        self.dir_x = 0.7
+        self.x, self.y = screen.WIDTH // 2 + 300, screen.HEIGHT  # 초기 위치 (화면 하단 중앙)
+        self.isJump = 0  # 점프 확인
+        self.v = VELOCITY  # 속도
+        self.m = MASS  # 질량
+
+    def draw(self):
+        self.image.clip_draw(400, 0, 100, 60, self.x, self.y)
+
+    def update(self):
+        if self.y > 35:
+            self.x += self.dir_x * 1
+            if self.isJump == 0:
+                self.y -= 1
+        if self.isJump > 0:
+
+            # if self.isJump == 2: # 이단 점프
+            #     self.v = VELOCITY
+
+            # 역학공식 계산 (F). F = 0.5 * mass * velocity^2.
+            if self.v > 0:  # 속도가 0보다 클때는 위로 올라감
+                F = -(0.005 * self.m * (self.v ** 2))
+            else:  # 속도가 0보다 작을때는 아래로 내려감
+                F = (0.005 * self.m * (self.v ** 2))
+
+            self.y -= round(F)  # 좌표 반영하기
+
+            self.v -= 0.01  # 속도 줄이기
+
+        if self.y <= 35:  # 바닥에 닿았을때 변수 리셋
+            self.y = 35
+            self.isJump = 1
+            self.v = VELOCITY
+
+    def screen_check(self):
+        if self.x > screen.WIDTH:
+            self.x = screen.WIDTH
+            self.dir_x = -0.8
+
+        elif self.x < 0:
+            self.x = 0
+            self.dir_x = 0.8
+
+        elif self.y < 35:
+            self.y = 35
+
+        elif self.y > screen.HEIGHT:
+            self.y = screen.HEIGHT - 10
 
 def handle_events():
     global playing, mario
@@ -136,29 +223,55 @@ playing = True
 mario = None
 spawnmush = 0
 mushroom = None
+spawnflower = 0
+flower = None
+spawnstar = 0
+star = None
+tile = None
+
+def collider(mario, item):
+    pass
+
+def get_item():
+    if (collider(mario, mushroom) == 1) and (mario.state != 'flower'):
+        mario.state = 'mushroom'
+        game_framework.quit()
+        mario.life = 2
+    if (collider(mario, flower) == 1):
+        mario.state = 'flower'
+        mario.life = 3
+    if (collider(mario, star) == 1):
+        mario.state = 'star'
+
 
 def enter():
-    global mario, playing, mushroom, spawnmush
+    global mario, playing, mushroom, flower, star, tile
+    tile = Tile()
     mario = Mario()
-    if(spawnmush > 0):
-        mushroom = Mushroom()
+    mushroom = Mushroom()
+    flower = Fire_Flower()
+    star = Star()
     playing = True
 
 def update():
     global mario
     mario.update()
     mario.screen_check()
-    if (spawnmush > 0):
-        mushroom.update()
-        mushroom.screen_check()
-
+    mushroom.update()
+    mushroom.screen_check()
+    flower.update()
+    flower.screen_check()
+    star.update()
+    star.screen_check()
+    get_item()
 
 
 def draw():
     clear_canvas()
     mario.draw()
-    if (spawnmush > 0):
-        mushroom.draw()
+    mushroom.draw()
+    flower.draw()
+    star.draw()
     update_canvas()
 
 def exit():
