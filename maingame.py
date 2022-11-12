892190135464e460ab189479bb5965741d4ecb3a
Project-Blossom <112834007+Project-Blossom@users.noreply.github.com>
2022-11-12 20:35:00 +0900

    타이틀 추가

--- maingame.py
+++ maingame.py
@@ -1,9 +1,10 @@
 from pico2d import *
 import game_framework
 import play_state
+import title_state
 
 WIDTH, HEIGHT = 1400, 700
 
 open_canvas(WIDTH, HEIGHT)
-game_framework.run(play_state)
+game_framework.run(title_state)
 close_canvas()
---
+++ title_state.py
@@ -0,0 +1,38 @@
+from pico2d import *
+import game_framework
+import play_state
+
+running = True
+image = None
+
+
+def enter():
+    global image
+    image = load_image('title.png')
+    pass
+
+def exit():
+    global image
+    del image
+    # fill here
+    pass
+
+def update():
+    pass
+
+def draw():
+    clear_canvas()
+    image.draw(1400 // 2, 700 // 2)
+    update_canvas()
+    # fill here
+    pass
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+            game_framework.quit()
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
+            game_framework.change_state(play_state)
